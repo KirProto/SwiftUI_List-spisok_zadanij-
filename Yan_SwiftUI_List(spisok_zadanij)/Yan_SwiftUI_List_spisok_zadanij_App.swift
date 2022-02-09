ec86c34cbceee82c16a32ea5e13ff5a623c0f19f
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 12:18:51 +0500

    Initial Commit

---
+++ Yan_SwiftUI_List(spisok_zadanij)/Yan_SwiftUI_List_spisok_zadanij_App.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_List_spisok_zadanij_App.swift
+//  Yan_SwiftUI_List(spisok_zadanij)
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_List_spisok_zadanij_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
